36fd0a3dd9e6ebff6dbb157e78c5a50805320323
Jisung15 <gimseongjin302@gmail.com>
2024-08-09 21:48:04 +0900

    코드 수정

--- app/src/main/java/com/example/mybmi_calculator/ResultActivity.kt
+++ app/src/main/java/com/example/mybmi_calculator/ResultActivity.kt
@@ -3,7 +3,6 @@ package com.example.mybmi_calculator
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
